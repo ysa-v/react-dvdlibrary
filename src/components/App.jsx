0f2cc2297c8393441077736ee9e556bbf0608fbb
ysa-v <ysa.vandenberg@gmail.com>
2022-12-16 10:41:19 -0600

    Routing added to index and app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Contact from "./Contact.jsx";
 import Search from "./Search";
 function App() {
   return (
-    <div>
+    <>
+      <Routes>
+        <Route path="/" element={<Search />}/>
+        {/* <Route path="/:dvdID" element={<DvdDetails />} /> */}
+        {/* <Route path="/add" element={<AddDvd />} /> */}
+      </Routes>
       <Search />
-    </div>
+    </>
   );
 }
 
